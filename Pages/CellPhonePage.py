431327dfb75d51acc329f857c3345a401bd09b93
srialok01 <MS7@drive>
2019-10-08 02:45:16 +0530

    Added Lambda function for Selenium WebDriver wait

--- Pages/CellPhonePage.py
+++ Pages/CellPhonePage.py
@@ -1,5 +1,7 @@
 import time
 
+from selenium.webdriver.support.wait import WebDriverWait
+
 
 class CellPhonePage():
     def __init__(self, driver):
@@ -7,15 +9,16 @@ class CellPhonePage():
 
         self.Nokia_Phone_Linktxt = "Nokia Lumia 1020"
         self.Add_to_Cart_xpath = "//input[@type='button'][@id='add-to-cart-button-20']"
-        self.add_to_cart_xpath = "//div[@class='header-links']//descendant::span[@class ='cart-label']"
+        self.shoppingCart_xpath = "//div[@class='header-links']//descendant::span[@class ='cart-label']"
 
     def SelectPhone(self):
         self.driver.find_element_by_partial_link_text(self.Nokia_Phone_Linktxt).click()
         self.driver.execute_script("window.scrollTo(0, 400);")
         time.sleep(3)
         self.driver.find_element_by_xpath(self.Add_to_Cart_xpath).click()
-        time.sleep(7)
         self.driver.execute_script("window.scrollTo(0, -400);")
-        self.driver.find_element_by_xpath(self.add_to_cart_xpath).click()
+        cart_button = WebDriverWait(self.driver, 10).until(lambda s: s.find_element_by_xpath(self.shoppingCart_xpath))
+        cart_button.click()
+
 
 
